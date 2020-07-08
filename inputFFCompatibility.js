28de4306d945b36f29b346c53b4a9ae6a89689dc
Inferysp-LinuxMint <kacperkrolak@gmail.com>
2020-07-08 13:07:43 +0200

    Responsiveness, input range for Fire fox jQuery fix, style details change.

---
+++ inputFFCompatibility.js
@@ -0,0 +1,1 @@
+$('input,textarea').attr('autocomplete', 'off');
--- script.js
+++ script.js
@@ -1,45 +1,51 @@
-const slideTown = document.querySelector("#town-driving");
-const slideLongDist = document.querySelector("#long-distance-driving");
-const valueTown = document.querySelector(".town-travel");
-const valueLongDist = document.querySelector(".long-distance");
-const whitchTechnologyResult = document.querySelector(".compare-result");
-const savedMoney = document.querySelector(".economy-result");
+document.addEventListener("DOMContentLoaded",  function() {
+    const slideTown = document.querySelector("#town-driving");
+    const slideLongDist = document.querySelector("#long-distance-driving");
+    const valueTown = document.querySelector(".town-travel");
+    const valueLongDist = document.querySelector(".long-distance");
+    const whitchTechnologyResult = document.querySelector(".compare-result");
+    const savedMoney = document.querySelector(".economy-result");
 
-var combustionTravel = 0;
-var hybridTravel = 0;
-var combustionTown = 0;
-var hybridTown = 0;
+    let combustionTravel = 0;
+    let hybridTravel = 0;
+    let combustionTown = 0;
+    let hybridTown = 0;
 
+    const combTownConst = 4.5 * 8 / 100;
+    const hybrTownConst = 4.5 * 4 / 100;
+    const combTravConst = 4.5 * 5 / 100;
+    const hybrTravConst = 4.5 * 7 / 100;
 
-const townSliderFunction = (e) => {
-    combustionTown = e.target.value * 4.5 * 8 / 100;
-    hybridTown = e.target.value * 4.5 * 4 / 100;
-    valueTown.innerText = `${e.target.value} km`;
-    compareResultFunction();
-}
-slideTown.addEventListener("input", townSliderFunction);
-
-const travelSliderFunction = (e) => {
-    combustionTravel = e.target.value * 4.5 * 5 / 100;
-    hybridTravel = e.target.value * 4.5 * 7 / 100;
-    valueLongDist.innerText = `${e.target.value} km`;
-    compareResultFunction();
-}
-slideLongDist.addEventListener("input", travelSliderFunction);
-
-const compareResultFunction = () => {
-    let combustion = combustionTown + combustionTravel;
-    let hybrid = hybridTown + hybridTravel;
-    var moneyEarned = 0;
-    if (combustion < hybrid) {
-        whitchTechnologyResult.innerText = 'SPALINOWY';
-        moneyEarned = hybrid - combustion;
-    } else if (combustion > hybrid) {
-        whitchTechnologyResult.innerText = 'HYBRYDOWY';
-        moneyEarned = combustion - hybrid;
-    } else {
-        whitchTechnologyResult.innerText = 'obydwa opłacają się tak samo';
-        savedMoney.innerHTML = 0;
+    const townSliderFunction = (e) => {
+        combustionTown = e.target.value * combTownConst;
+        hybridTown = e.target.value * hybrTownConst;
+        valueTown.innerText = `${e.target.value} km`;
+        compareResultFunction();
     }
-    savedMoney.innerHTML = Math.floor(moneyEarned);
-}
+    slideTown.addEventListener("input", townSliderFunction);
+
+    const travelSliderFunction = (e) => {
+        combustionTravel = e.target.value * combTravConst;
+        hybridTravel = e.target.value * hybrTravConst;
+        valueLongDist.innerText = `${e.target.value} km`;
+        compareResultFunction();
+    }
+    slideLongDist.addEventListener("input", travelSliderFunction);
+
+    const compareResultFunction = () => {
+        let combustion = combustionTown + combustionTravel;
+        let hybrid = hybridTown + hybridTravel;
+        let moneySaved = 0;
+        if (combustion < hybrid) {
+            whitchTechnologyResult.innerText = 'SPALINOWY';
+            moneySaved = hybrid - combustion;
+        } else if (combustion > hybrid) {
+            whitchTechnologyResult.innerText = 'HYBRYDOWY';
+            moneySaved = combustion - hybrid;
+        } else {
+            whitchTechnologyResult.innerText = 'obydwa opłacają się tak samo';
+            savedMoney.innerHTML = 0;
+        }
+        savedMoney.innerHTML = Math.floor(moneySaved);
+    }
+});
